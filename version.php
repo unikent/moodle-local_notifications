06468119e0528ca132fa193a8ccc2fc1c0b7134d
Skylar Kelty <s.kelty@kent.ac.uk>
2015-06-29 16:49:58 +0100

    Bump version

--- version.php
+++ version.php
@@ -25,5 +25,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_notifications';
-$plugin->version   = 2015062504;
+$plugin->version   = 2015062900;
 $plugin->requires  = 2014051200;

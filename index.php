a78f2f87af88cee05cb8e9d56600a180da09d1ce
Skylar Kelty <s.kelty@kent.ac.uk>
2015-07-01 16:07:53 +0100

    Add back the custom notification form :)

--- index.php
+++ index.php
@@ -39,6 +39,23 @@ if ($action == 'delete') {
     redirect($PAGE->url, 'Notification deleted', 2);
 }
 
+// Check the form.
+$form = new \local_notifications\form\arbitrary();
+if ($data = $form->get_data()) {
+    \local_notifications\notification\arbitrary::create(array(
+        'objectid' => $data->courseid,
+        'context' => \context_course::instance($data->courseid),
+        'other' => array(
+            'actions' => $data->actions,
+            'level' => $data->type,
+            'dismissable' => $data->dismissable ? true : false,
+            'message' => $data->message
+        )
+    ));
+
+    redirect($PAGE->url, 'Notification created', 2);
+}
+
 $PAGE->requires->js_call_amd('local_notifications/filtering', 'init', array());
 
 $table = new \html_table();
@@ -88,7 +105,6 @@ foreach ($notifications as $row) {
 
 $notifications->close();
 
-
 // Output header.
 echo $OUTPUT->header();
 echo $OUTPUT->heading("Notifications Center");
@@ -114,6 +130,9 @@ echo \html_writer::table($table);
 
 $total = $DB->count_records('local_notifications', $notificationparams);
 echo $OUTPUT->paging_bar($total, $params['page'], $params['perpage'], $PAGE->url);
+
+// Add form.
+$form->display();
 
 // Output footer.
 echo $OUTPUT->footer();
